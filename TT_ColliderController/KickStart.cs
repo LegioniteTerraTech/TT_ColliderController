e3cd37c4dff4a89b8ece49ed563b48dee2416d05
Legionite <46542779+LegioniteTerraTech@users.noreply.github.com>
2021-06-21 13:10:18 -1000

    testing

--- TT_ColliderController/KickStart.cs
+++ TT_ColliderController/KickStart.cs
@@ -66,7 +66,7 @@ namespace TT_ColliderController
         public static void Main()
         {
             //Where the fun begins
-
+            
             //Initiate the madness
             HarmonyInstance harmonyInstance = HarmonyInstance.Create("legioniteterratech.collidercontroller");
             harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
